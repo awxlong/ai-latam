807bb33aa50194ddcda934870398fde09eb4252f
An Xuelong <60102826+awxlong@users.noreply.github.com>
2023-01-08 23:06:33 -0500

    Create variables-ver-python.py

---
+++ capítulo-I/variables-ver-python.py
@@ -0,0 +1,36 @@
+# Fundamentos de programacion: variables
+# Variables primitivos y referenciales
+# sintaxis es:
+# nombre_variable = expresion o valor_de_variable
+
+# VARIABLES DE TIPO PRIMITIVO       
+x = 5  # enteros
+miVariable = -10
+y = 5 * -8  # y = 40
+
+miDecimal = 3.14 # decimal 
+
+miCaracter = 'a' # caracter
+miCaracterMay = 'G'
+
+verdad = True  # booleana
+otroValor = False 
+expresionBool = 5 > 3  # True
+z = -9 > 8  # False
+
+# VARIABLES DE TIPO REFERENCIALES 
+
+mensaje = "Hola, Mundo"
+miListaVacia = [] 
+miListaConNum = [1, 2, 7, 9]  # lista de enteros
+miListaDiversa = [x, y, miDecimal]
+tablaPeriodica = {
+    "Sodio": "Na",
+    "Carbono": "C", 
+    "Helio": "He"
+}
+
+
+# class
+
+print(miListaDiversa)
